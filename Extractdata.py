bab8e42674af6584443b87f9869914bfa68acfb6
spiderLambo <spiderLambo.dev@outlook.com>
2025-02-08 18:04:24 +0100

    identification par un id

--- Extractdata.py
+++ Extractdata.py
@@ -5,23 +5,34 @@ repd1, repd2 = 0, 0 # Réponse pour les données à analyser
 
 def printChooseDico(dict): # Fonction pour afficher clées possibles dictionnaire
     i = 1
+    listeChoix = [] # Liste qui contient la valeur des choix possibles
     print("Choisir une clée")
     for key in dict.keys():
         print(f"{i}.{key}")
         i += 1
+        listeChoix.append(key)
+    return listeChoix
 
 with open('qualite-de-lair-mesuree-dans-la-station-chatelet-rer-a0.csv') as fichier_csv:
     reader = csv.DictReader(fichier_csv, delimiter=';')
 
     for ligne in reader:
+        # Permettre à l'utisisateu de faire un choix au premier tour de bouxhe
         while not repd2 in ligne:  # Redemander tant que la réponse 2 n'est pas dans le champ proposé
-            printChooseDico(ligne)
+
+            listeChoix = printChooseDico(ligne)
+
             if repd1 in ligne:  # Pour remplir repd1 en premier
-                repd2 = input()
+                repd2 = int(input())
+                if repd2 - 1 in range(len(listeChoix)): # Associer un numéro à une clée
+                    repd2 = listeChoix[repd2 - 1] # replacer l'id par le choix
             else:
-                repd1 = input()
+                repd1 = int(input())
+                if repd1 - 1 in range(len(listeChoix)): # Associer un numéro à une clée
+                    repd1 = listeChoix[repd1 - 1]  # replacer l'id par le choix
 
 
+        # Ajouter l'élément
         data1.append(ligne[repd1])
         data2.append(ligne[repd2])
 
