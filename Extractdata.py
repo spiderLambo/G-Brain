cbb87a9f170f1055c40ee8deb848b0383a78b799
spiderLambo <spiderLambo.dev@outlook.com>
2025-02-08 17:50:52 +0100

    ajout choix de donné

--- Extractdata.py
+++ Extractdata.py
@@ -1,12 +1,29 @@
 import csv
 
-data1, data2 = [], []
+data1, data2 = [], [] # Listes qui contiendrons les données à extraire
+repd1, repd2 = 0, 0 # Réponse pour les données à analyser
+
+def printChooseDico(dict): # Fonction pour afficher clées possibles dictionnaire
+    i = 1
+    print("Choisir une clée")
+    for key in dict.keys():
+        print(f"{i}.{key}")
+        i += 1
 
 with open('qualite-de-lair-mesuree-dans-la-station-chatelet-rer-a0.csv') as fichier_csv:
     reader = csv.DictReader(fichier_csv, delimiter=';')
+
     for ligne in reader:
-        data1.append(ligne["\ufeffdate/heure"])
-        data2.append(ligne["TEMP"])
+        while not repd2 in ligne:  # Redemander tant que la réponse 2 n'est pas dans le champ proposé
+            printChooseDico(ligne)
+            if repd1 in ligne:  # Pour remplir repd1 en premier
+                repd2 = input()
+            else:
+                repd1 = input()
+
+
+        data1.append(ligne[repd1])
+        data2.append(ligne[repd2])
 
 
 if __name__ == "__main__":
