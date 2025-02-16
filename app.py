46227bff872ee85d3259215e9b868a51232f06a3
spiderLambo <spiderLambo.dev@outlook.com>
2025-02-17 00:49:16 +0100

    elt without search

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ from bddGestion import *
 # Création de l'application
 app = Flask(__name__, static_url_path="/static")
 
+
 # Page principale
-@app.route("/") # Présision du chemin
+@app.route("/")  # Présision du chemin
 def home():
-    return render_template('g-brain_principal.html')
+    return render_template("g-brain_principal.html")
 
 
 # Page de recherche
 @app.route("/search")
 def searchapp():
-    return render_template('g-brain_recherche.html')
-
+    return render_template("g-brain_recherche.html", search=["a"])
 
 
 if __name__ == "__main__":
-    app.run(debug=True) # Lancement de l'application
+    app.run(debug=True)  # Lancement de l'application
