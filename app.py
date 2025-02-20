7648e1a31dc0ebccc5d854550ef8af04bcaadcd4
spiderLambo <spiderLambo.dev@outlook.com>
2025-02-20 20:35:37 +0100

    ajout connexion page

--- app.py
+++ app.py
@@ -40,6 +40,12 @@ def graph(graphName):
     return render_template("g-brain_graphe.html", info = infos)
 
 
+# Page de connexion
+@app.route("/connexion")
+def connect():
+    return render_template('g-brain_connexion.html')
+
+
 # Page d'erreur
 @app.errorhandler(404)
 def page_not_found(e):
