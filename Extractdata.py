7057b34aca42bb9cf51bbdd973d6a4fb8e3c3911
spiderLambo <spiderLambo.dev@outlook.com>
2025-03-02 15:48:49 +0100

    rename file

--- Extractdata.py
+++ Extractdata.py
@@ -1,4 +1,5 @@
 import csv
+import datetime
 
 data1, data2 = [], [] # Listes qui contiendrons les données à extraire
 repd1, repd2 = 0, 0 # Réponse pour les données à analyser
@@ -13,11 +14,10 @@ def printChooseDico(dict): # Fonction pour afficher clées possibles dictionnair
         listeChoix.append(key)
     return listeChoix
 
-with open('qualite-de-lair-mesuree-dans-la-station-chatelet-rer-a0.csv') as fichier_csv:
+with open('vacsi12-dep-2022-01-26-19h10.csv') as fichier_csv:
     reader = csv.DictReader(fichier_csv, delimiter=';')
-    i=0
     for ligne in reader:
-        # Permettre à l'utisisateur de faire un choix au premier tour de bouxhe
+        # Permettre à l'utisisateur de faire un choix au premier tour de boucle
         while not repd2 in ligne:  # Redemander tant que la réponse 2 n'est pas dans le champ proposé
 
             listeChoix = printChooseDico(ligne)
@@ -36,12 +36,28 @@ with open('qualite-de-lair-mesuree-dans-la-station-chatelet-rer-a0.csv') as fich
         ligne[repd2] = ligne[repd2].replace(",", ".")
 
 
-        # Ajouter l'élément si il peur être convertis
+        # Ajouter l'élément si il s'agit de deux réels
         if ligne[repd1].replace(".", "").isdigit() and ligne[repd2].replace(".", "").isdigit():
             data2.append(float(ligne[repd2]))
             data1.append(float(ligne[repd1]))
 
+        # Ajouter l'élément si il s'agit d'un réel et une date au format 2025-01-01
+        elif "-" in ligne[repd1] and len(ligne[repd1]) == 10 and ligne[repd2].replace(".", "").isdigit():
+            data2.append(float(ligne[repd2]))
+            data1.append(datetime.datetime.strptime(ligne[repd1], "%Y-%m-%d"))
 
-if __name__ == "__main__":
-    print("data1:", len(data1))
-    print("data2:", len(data2))
+        # Ajouter l'élément si il s'agit d'un réel et une date au format 01/01/2025
+        elif "/" in ligne[repd1] and ligne[repd2].replace(".", "").isdigit():
+            data2.append(float(ligne[repd2]))
+            data1.append(datetime.datetime.strptime(ligne[repd1], "%d/%m/%Y"))
+
+        # Ajouter l'élément si il s'agit d'un réel et une date au format January 1, 2025
+        elif "," in ligne[repd1] and ligne[repd2].replace(".", "").isdigit():
+            data2.append(float(ligne[repd2]))
+            data1.append(datetime.datetime.strptime(ligne[repd1], "%B %d, %Y"))
+
+        # Ajouter l'élément si il s'agit d'un réel et une date au format 01-01-2025 00:00
+        elif "-" in ligne[repd1] and ligne[repd2].replace(".", "").isdigit():
+            data2.append(float(ligne[repd2]))
+            data1.append(datetime.datetime.strptime(ligne[repd1], "%d-%m-%Y %H:%M"))
+
